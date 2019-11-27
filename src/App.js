1c04d3d4bc51d0c703563bc0df7e313dff4f87b9
SD Peralta <sdperalta@99.200-57-1.bestelclientes.com.mx>
2019-11-27 14:58:14 -0600

    Changing BrowserRouterb for HashRouter

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import LogIn from "./Components/LogIn";
 import SignUp from "./Components/SignUp";
 import NotesTimeline from "./Components/NotesTimeline";
-import {BrowserRouter as Router, Route} from "react-router-dom";
+import {HashRouter as Router, Route} from "react-router-dom";
 import { AuthProvider } from "./Components/AuthState";
 import PrivateRoute from "./Components/PrivateRoute";
 import "./App.css";
@@ -10,7 +10,7 @@ import "./App.css";
 const App = () => {
   return (
     <AuthProvider>
-      <Router>
+      <Router basename="/GDL003-lab-notes/">
         <Route exact path="/" component={LogIn} />
         <Route exact path="/signup" component={SignUp} />
         <PrivateRoute exact path="/notes" component={NotesTimeline} />
